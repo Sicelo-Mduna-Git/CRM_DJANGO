6b5fa0ef947c898d7fbb29732fd1dcc97a549118
Sicelo Mduna <mdunasicelo@gmail.com>
2025-04-18 21:14:27 +0200

    rendered my first html template

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("Hello, world. You're at the webapp home page.")
+  return render(request, 'webapp/index.html') 
