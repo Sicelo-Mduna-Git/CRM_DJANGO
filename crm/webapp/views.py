91b0023dd5b2d5df5cddc66fc0dfdf8d02710483
Sicelo Mduna <mdunasicelo@gmail.com>
2025-04-21 17:43:40 +0200

    Configured my logout page

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
-
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 # Home page / Landing page
 def home(request):
   return render(request, 'webapp/index.html') 
@@ -18,5 +19,32 @@ def register(request):
 
   context = {'form': form}
   return render(request, 'webapp/register.html', context = context)
+
+#login a user
+
+def login(request):
+  form = LoginForm()
+  if request.method == "POST":
+    form = LoginForm(request, data = request.POST)
+    
+    if form.is_valid():
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      
+      user = authenticate(request, username=username, password=password)
+      
+      if user is not None:
+        auth.login(request, user)
+        # return redirect('login')
+        
+  context = {'form': form}
+  return render(request, 'webapp/login.html', context = context)
+
+
+# logout a user
+
+def logout(request):
+  auth.logout(request)
+  return redirect('login')
 
   
